dd5648abb2127411eabc6cc75725f9c1b94e36bc
Lorenzo Ferrara Junior <lorenzoferrarajr@gmail.com>
2012-09-21 08:17:58 +0200

    Removed unneeded getAutoloaderConfig

--- Module.php
+++ Module.php
@@ -13,17 +13,6 @@ class Module
     {
         return include __DIR__ . '/config/module.config.php';
     }
-
-    public function getAutoloaderConfig()
-    {
-        return array(
-            'Zend\Loader\StandardAutoloader' => array(
-                'namespaces' => array(
-                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
-                ),
-            ),
-        );
-    }
     
     /**
      * 
