f57b4c02c63510c938dcdda44803fb322e89477f
Lorenzo Ferrara Junior <lorenzoferrarajr@gmail.com>
2012-11-18 16:00:38 +0100

    PHP coding standards
    php-cs-fixer fix .

--- Module.php
+++ Module.php
@@ -8,32 +8,32 @@ use Zend\View\Model;
 class Module
 {
     protected static $layout;
-    
+
     public function getConfig()
     {
         return include __DIR__ . '/config/module.config.php';
     }
-    
+
     /**
-     * 
+     *
      * @param \Zend\Mvc\MvcEvent $e
      */
-    public function onBootstrap(Event $e) {
+    public function onBootstrap(Event $e)
+    {
         // $e->getTarget() is application
         $events = $e->getTarget()->getEventManager();
-        
+
         $events->attach('dispatch.error', function($ev) use ($e) {
-            
+
             $layout   = new Model\ViewModel();
             $layout->setTerminal(true);
             $layout->setTemplate('layout/error');
             $result = $e->getResult();
-            
+
             $layout->addChild($result, 'content');
             $e->setResult($layout);
-            
+
         });
     }
 
 }
-
